5e9d36f119c1792c7d7e195c0e18f302e658d4e4
Raisa Rodrigues <raisa.lobo@gmail.com>
2026-01-15 14:23:51 -0600

    f1 feature

---
+++ src/main/java/com/qa/nge/pages/Feature2Page.java
@@ -0,0 +1,11 @@
+package com.qa.nge.pages;
+
+public class Feature2Page {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is feature2 test");
+
+	}
+
+}
---
+++ src/main/java/com/qa/nge/pages/TestPage.java
@@ -0,0 +1,9 @@
+package com.qa.nge.pages;
+
+public class TestPage {
+
+	public static void main(String[] args)
+	{
+		System.out.println("raisa testing code");
+	}
+}

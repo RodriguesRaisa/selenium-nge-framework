15381ce80e7f8ee1a61bbf8097431542e37636a2
Raisa Rodrigues <raisa.lobo@gmail.com>
2026-01-15 14:46:02 -0600

    deleted the files

--- src/main/java/com/qa/nge/pages/Rest.java
+++
@@ -1,9 +0,0 @@
-package com.qa.nge.pages;
-
-public class Rest {
-
-	public static void main(String[] args)
-	{
-		System.out.println("richa testing code");
-	}
-}
--- src/main/java/com/qa/nge/pages/TestPage.java
+++
@@ -1,9 +0,0 @@
-package com.qa.nge.pages;
-
-public class TestPage {
-
-	public static void main(String[] args)
-	{
-		System.out.println("raisa testing code");
-	}
-}
